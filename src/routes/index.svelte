1fe0340a1464e3adc1d9d42e7cb864f28394517d
John-Kim <john@leftium.com>
2021-11-25 22:19:30 +0900

    Also style query input.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -52,6 +52,25 @@
         width: 110px;
     }
 
+    input, button, select, textarea {
+        font-family: inherit;
+        font-size: inherit;
+        padding: 0.4em;
+        margin: 0 0 0.5em 0;
+        box-sizing: border-box;
+        border: 1px solid #ccc;
+        border-radius: 2px;
+    }
+
+    input {
+        width: 100%;
+        margin-top: 4px;
+
+        border-top-left-radius: 0;
+        border-bottom-left-radius: 0;
+        border-top-right-radius: 2px;
+        border-bottom-right-radius: 2px;
+    }
 </style>
 
 <svelte:body on:paste={handlePaste} />
