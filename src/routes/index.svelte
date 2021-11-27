066d4dd809eeaba14b63aab3e3bda30d050088ea
John-Kim <john@leftium.com>
2021-11-27 13:40:04 +0900

    Add HODL bot calculator.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -23,9 +23,15 @@
     `let parsedQuery = null`
     `let formattedParsedQuery = null`
 
+    `let formattedParsedQueryComponent = null`
+
     `let satoshiFromBtcComponent = null`
     `let satoshiFromUsdComponent = null`
     `let satoshiFromKrwComponent = null`
+
+    `let hodlBotLowPriceComponent = null`
+    `let hodlBotHighPriceComponent = null`
+    `let hodlBotTrailingDownComponent = null`
 
     `export let query = ''`
 
@@ -44,14 +50,24 @@
     getSatoshiFromUsd = (q) -> convertWithRate q, satsPerUsd
     getSatoshiFromKrw = (q) -> convertWithRate q, satsPerKrw
 
+    getHodlBotLowPrice     = (q) -> q * (1 - 0.0037*22)
+    getHodlBotHighPrice    = (q) -> q * (1 + 0.0037*7)
+    getHodlBotTrailingDown = (q) -> getHodlBotLowPrice(q) * 0.9
+
     handlePaste = (e) ->
         data = (event.clipboardData || window.clipboardData).getData('text');
         if data
             inputQuery.value = query = data
             parsedQuery = parseQuery data
+            formattedParsedQueryComponent.textContent = parsedQuery.toLocaleString()
+
             satoshiFromBtcComponent.value = getSatoshiFromBtc(parsedQuery)
             satoshiFromUsdComponent.value = getSatoshiFromUsd(parsedQuery)
             satoshiFromKrwComponent.value = getSatoshiFromKrw(parsedQuery)
+
+            hodlBotLowPriceComponent.value     = getHodlBotLowPrice(parsedQuery)
+            hodlBotHighPriceComponent.value    = getHodlBotHighPrice(parsedQuery)
+            hodlBotTrailingDownComponent.value = getHodlBotTrailingDown(parsedQuery)
 
     nullHandler = (e) -> console.log '(nullHandler)'
 
@@ -59,7 +75,7 @@
 
 <style>
     :global(.results button) {
-        width: 110px;
+        width: 130px;
     }
 
     input {
@@ -89,29 +105,57 @@
            on:paste|preventDefault={nullHandler} />
 </div>
 
-<div>
+<div bind:this={formattedParsedQueryComponent}>
     {formattedParsedQuery}
 </div>
 
 <div class="results">
-    <div>
-        <ClipboardInput
-            bind:this={satoshiFromBtcComponent}
-            label='BTC&#8680;SAT'
-            value={getSatoshiFromBtc(parsedQuery)} />
-    </div>
+    <fieldset>
+        <legend>Satoshi</legend>
+        <div>
+            <ClipboardInput
+                bind:this={satoshiFromBtcComponent}
+                label='BTC&#8680;SAT'
+                value={getSatoshiFromBtc(parsedQuery)} />
+        </div>
 
-    <div>
-        <ClipboardInput
-            bind:this={satoshiFromUsdComponent}
-            label='USD&#8680;SAT'
-            value={getSatoshiFromUsd(parsedQuery)} />
-    </div>
+        <div>
+            <ClipboardInput
+                bind:this={satoshiFromUsdComponent}
+                label='USD&#8680;SAT'
+                value={getSatoshiFromUsd(parsedQuery)} />
+        </div>
 
-    <div>
-        <ClipboardInput
-            bind:this={satoshiFromKrwComponent}
-            label='KRW&#8680;SAT'
-            value={getSatoshiFromKrw(parsedQuery)} />
-    </div>
+        <div>
+            <ClipboardInput
+                bind:this={satoshiFromKrwComponent}
+                label='KRW&#8680;SAT'
+                value={getSatoshiFromKrw(parsedQuery)} />
+        </div>
+    </fieldset>
+
+    <fieldset>
+        <legend>HODL Bot</legend>
+        <div>
+            <ClipboardInput
+                bind:this={hodlBotLowPriceComponent}
+                label='Low Price'
+                value={getHodlBotLowPrice(parsedQuery)} />
+        </div>
+
+        <div>
+            <ClipboardInput
+                bind:this={hodlBotHighPriceComponent}
+                label='High Price'
+                value={getHodlBotHighPrice(parsedQuery)} />
+        </div>
+
+        <div>
+            <ClipboardInput
+                bind:this={hodlBotTrailingDownComponent}
+                label='Trailing Down'
+                value={getHodlBotTrailingDown(parsedQuery)} />
+        </div>
+    </fieldset>
+
 </div>
